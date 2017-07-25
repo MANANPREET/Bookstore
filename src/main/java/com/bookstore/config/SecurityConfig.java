fd96a4632426fa7a76b603a465dd05393c949d53
MANANPREET <mananpreetsingh.win@gmail.com>
2017-07-25 10:54:58 +0530

    Security configuration added. Files added are : UserRepository.java, UserSecurityService.java, SecurityConfig.java

--- src/main/java/com/bookstore/config/SecurityConfig.java
+++ src/main/java/com/bookstore/config/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import com.bookstore.service.impl.UserSecurityService;
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled=true)
---
+++ src/main/java/com/bookstore/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.bookstore.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bookstore.domain.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+	User findByUsername(String username);
+}
